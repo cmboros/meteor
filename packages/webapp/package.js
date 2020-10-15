fa44792b11e71118620b450465802592efc52b77
cmboros <costineelmarin@gmail.com>
2020-10-15 08:56:16 -0400

    Update package.js

--- packages/webapp/package.js
+++ packages/webapp/package.js
@@ -20,7 +20,7 @@ Npm.strip({
 });
 
 Cordova.depends({
-  'cordova-plugin-whitelist': '1.3.4',
+  //'cordova-plugin-whitelist': '1.3.4',
   'cordova-plugin-meteor-webapp': 'https://github.com/cmboros/cordova-plugin-meteor-webapp.git#ed95ee67eac91ec49d9e72af86e8e9eb2baec935'
 });
 
